83bea7a872a5a252bd48758acb959f86de25dd1c
Evan Anger <devandanger@gmail.com>
2025-07-02 23:27:51 -0400

    Initial Commit

---
+++ airhockey-iosTests/airhockey_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  airhockey_iosTests.swift
+//  airhockey-iosTests
+//
+//  Created by Evan Anger on 7/2/25.
+//
+
+import Testing
+@testable import airhockey_ios
+
+struct airhockey_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
